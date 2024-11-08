faad2a6d910d183a3179c1c23c30839f6861ed9e
xLillium <nicolas.motillon@gmail.com>
2024-11-08 15:00:43 +0100

    init: basic layout + simple get request

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/xlillium/go-postman-tui
+
+go 1.23.2
+
+require (
+	github.com/gdamore/tcell/v2 v2.7.4
+	github.com/rivo/tview v0.0.0-20241103174730-c76f7879f592
+)
+
+require (
+	github.com/gdamore/encoding v1.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/term v0.26.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "log"
+
+    "github.com/rivo/tview"
+    "github.com/gdamore/tcell/v2"
+    "github.com/xlillium/go-postman-tui/handlers"
+    "github.com/xlillium/go-postman-tui/ui"
+)
+
+func main() {
+    app := tview.NewApplication()
+    ui := ui.NewUI(app)
+
+    // Event handler for the input field
+    ui.TopInput.SetDoneFunc(func(key tcell.Key) {
+        if key == tcell.KeyEnter {
+            url := ui.TopInput.GetText()
+            go func() {
+                handlers.PerformGetRequest(url, ui.MiddleBox, ui.BottomBox, ui.App)
+            }()
+        }
+    })
+
+    // Start the application
+    if err := app.SetRoot(ui.Flex, true).SetFocus(ui.TopInput).Run(); err != nil {
+        log.Fatalf("Error running application: %v", err)
+    }
+}
+
