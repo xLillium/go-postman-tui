581a3f5e4f637bab84930474fe8219a30a2e884a
xLillium <nicolas.motillon@gmail.com>
2024-11-09 01:42:41 +0100

    Enable mouse navigation

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,15 +1,14 @@
 package app
 
 import (
-    "github.com/rivo/tview"
-    "github.com/xlillium/go-postman-tui/internal/ui"
+	"github.com/rivo/tview"
+	"github.com/xlillium/go-postman-tui/internal/ui"
 )
 
 func Run() error {
-    app := tview.NewApplication()
-    ui := ui.Initialize(app)
+	app := tview.NewApplication()
+	ui := ui.Initialize(app)
 
-    // Start the application
-    return app.SetRoot(ui.RootLayout, true).SetFocus(ui.InitialFocus).Run()
+	// Start the application
+	return app.SetRoot(ui.RootLayout, true).SetFocus(ui.InitialFocus).EnableMouse(true).Run()
 }
-
